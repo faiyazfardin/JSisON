9d0cf686f75048a34e1efc5716abde15b9503728
Faiyaz Fardin <74107819+faiyazfardin@users.noreply.github.com>
2025-11-26 20:41:44 +0600

    3rd commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let username, age, hobby;
+
+username = prompt("Enter your name: ");
+age = prompt("Enter your age: ");
+hobby = prompt("Enter your hobby: ");
+
+console.log("User Information:");
+console.log("Name: " + username);
+console.log("Age: " + age);
+console.log("Hobby: " + hobby);
+
+
+document.write(username.charAt(3));
